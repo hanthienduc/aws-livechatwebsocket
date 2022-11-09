7cc4420d8d13d28d439a2b22cf6694676bd4147b
hanthienduc <hanthienduc.96@gmail.com>
2022-11-09 18:25:11 +0900

    change region

--- serverless.ts
+++ serverless.ts
@@ -10,7 +10,7 @@ const serverlessConfiguration: AWS = {
     provider: {
         name: 'aws',
         runtime: 'nodejs14.x',
-        region: 'eu-central-1',
+        region: 'us-east-1',
         profile: 'serverlessUser',
         iamRoleStatements: [
             {
