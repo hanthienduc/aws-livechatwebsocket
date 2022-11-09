3ace911562b44e6fa5c6b1b760cc6769bb087d5b
hanthienduc <hanthienduc.96@gmail.com>
2022-11-09 15:53:48 +0900

    add send reminder endpoint code

--- src/functions/sendReminder/index.ts
+++ src/functions/sendReminder/index.ts
@@ -1,10 +1,26 @@
 import { DynamoDBStreamEvent } from 'aws-lambda'
+import { unmarshall } from '@aws-sdk/util-dynamodb'
+import { AttributeValue } from '@aws-sdk/client-dynamodb'
 
 export const handler = async (event: DynamoDBStreamEvent) => {
     try {
-       
+        const reminderPromises = event.Records.map(async (record) => {
+            const data = unmarshall(
+                record.dynamodb.OldImage as Record<string, AttributeValue>
+            )
+
+            const { email, phoneNumber, reminder } = data
+
+            if (phoneNumber) {
+                await sendSMS({ phoneNumber, reminder })
+            }
+            if (email) {
+                await sendEmail({ email, reminder })
+            }
+        })
+
+        await Promise.all(reminderPromises)
     } catch (error) {
         console.log('error', error)
-       
     }
 }
