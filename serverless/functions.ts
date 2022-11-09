89b481e89c2fece1e93dec30d16c5776d91c4c09
hanthienduc <hanthienduc.96@gmail.com>
2022-11-09 16:45:59 +0900

    add custom permission for send action

--- serverless/functions.ts
+++ serverless/functions.ts
@@ -25,6 +25,14 @@ const functions: AWS['functions'] = {
                 },
             },
         ],
+        //@ts-expect-error
+        iamRoleStatements: [
+            {
+                Effect: 'Allow',
+                Action: ['ses:sendEmail', 'sns:Publish'],
+                Resource: '*',
+            },
+        ],
     },
 }
 
